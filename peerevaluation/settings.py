74facd732913b0da3167e534c30b7ad5f49add3b
Jose Escobar <joanesco@espol.edu.ec>
2020-08-29 20:58:17 -0500

    Requirements

--- peerevaluation/settings.py
+++ peerevaluation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x7d@7b-463fw*(oaf&c-2c*_$pzh8@*l3vwb*uvic==@)abz7b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["6efeb03a0e62.ngrok.io","localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
